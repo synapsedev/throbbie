f15bb092ad36bd641a3c5dcd9fd7898bd694923d
Remington <remington@synapse.com>
2010-03-09 20:39:43 -0800

    Throbby 2.0!
    o It actually throbs!

--- default/dep/throbby.o.d
+++ default/dep/throbby.o.d
@@ -10,7 +10,9 @@ throbby.o: ../throbby.c \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sleep.h
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sleep.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h
 
 c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
 
@@ -35,3 +37,7 @@ c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
 c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
 
 c:/winavr-20100110/lib/gcc/../../avr/include/avr/sleep.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
